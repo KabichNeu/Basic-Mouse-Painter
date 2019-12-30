63b09223930adea5fd522535d9d3a1bf6936b8e4
KabichNeu <kabichneu@gmail.com>
2019-12-30 23:44:56 +0545

    first commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+    background(250,350,100);
+
+}
+
+function draw() {
+  //ellipse
+  noStroke();
+  fill(200,300,400);
+  ellipse(mouseX,mouseY,25,25);
+  
+}
+function mousePressed(){
+  background(250,250,100)
+}
